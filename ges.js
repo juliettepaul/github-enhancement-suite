dd8894458473cc3febd4d1d5514450526f5e2fb1
JPaul <jaysonpaul@gmail.com>
2012-05-10 10:01:17 -0400

    Initial commit of GES

---
+++ ges.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name       GitHub Pull Request Enhancement Suite
+// @namespace  jpi
+// @version    0.1
+// @description  
+// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @match      https://github.etsycorp.com/*/pull/*
+// @match      https://github.etsycorp.com/*/commit/*
+// @copyright  2012
+// ==/UserScript==
+
+(function($) {
+    var filelist = new Array();
+
+    var parsePaths = function() {
+        $('td.path a').each(function (index) {
+            filelist[index] = new Array($(this).attr('href'), $(this).html());
+        });
+    }
+
+    var movePaths = function () {
+        $('#toc').wrapInner('<div style="position:fixed; width: 490px; top: 60px; left: 0px;" />');
+    }
+
+    var movePageRight = function () {
+        $('.hentry').css('margin', '0px 0px 0px 500px');
+    }
+
+    var renderPaths = function() {
+        parsePaths();
+        movePageRight();
+        movePaths();
+    }
+
+    var attachEvents = function() {
+        $.each(filelist, function (index, value) {
+        });
+    }
+
+    renderPaths();
+    attachEvents();
+})(jQuery);
+
