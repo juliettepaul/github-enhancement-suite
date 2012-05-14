6aa30f34a3eb3af233a4ff3c4198adc3aa756db9
JPaul <jaysonpaul@gmail.com>
2012-05-14 16:02:27 -0400

    Fixing match for regular github

--- ges.js
+++ ges.js
@@ -6,9 +6,8 @@
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @match      https://github.etsycorp.com/*/pull/*
 // @match      https://github.etsycorp.com/*/commit/*
-// @match      https://www.github.com/*/pull/*
-// @match      https://www.github.com/*/commit/*
-// @update_url https://raw.github.com/jayson/github-enhancement-suite/master/ges.js
+// @match      https://*github.com/*/pull/*
+// @match      https://*github.com/*/commit/*
 // @copyright  2012
 // ==/UserScript==
 
