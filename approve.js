a8c12f5a26e3eede9e0f2c38aebe4cc5d2f43d6f
JPaul <jaysonpaul@gmail.com>
2012-06-04 23:25:14 -0400

    Cleaning up approval container code a bit more

--- approve.js
+++ approve.js
@@ -46,7 +46,7 @@
 
     // Find the post comment form so we can approve/deny easily
     function findWriteBucket() {
-        write_bucket = $('#pull_comment_form [id^=write_bucket_] textarea').last();
+        write_bucket = $('#pull_comment_form [id^=write_bucket_] textarea');
         $('button.primary').prop('id', 'post-comment');
     }
 
@@ -92,12 +92,14 @@
         addApproveButtons();
     }
 
+    // Create the container if necessary to show approvals
     function prepareApprovalContainer() {
         if (!created_approval_container) {
             var discussion_timeline = $('div.discussion-timeline');
             discussion_timeline.prepend('<div class="new-comments"><div class="comment starting-comment"><div id="approvals" class="bubble"></div></div></div><br/>');
             approvals = $('#approvals'); 
             approvals.css('background', 'white');
+            created_approval_container = true;
         }
     }
 
@@ -111,7 +113,7 @@
             if (write_bucket.val().search(approval_regex) == -1) {
                 write_bucket.val('lgtm: ' + write_bucket.val());
             }
-            $('button#post-comment').last().click();
+            $('#pull_comment_form button#post-comment').click();
         });
 
         $('button.rejectit').on('click', function (e) {
@@ -119,7 +121,7 @@
             if (write_bucket.val().search(rejected_regex) == -1) {
                 write_bucket.val('rejected because: ' + write_bucket.val());
             }
-            $('button#post-comment').last().click();
+            $('#pull_comment_form button#post-comment').click();
         });
     }
 
