2db66ea64d7a527bfd2bcae21f5a1754d4794966
JPaul <jaysonpaul@gmail.com>
2012-06-07 13:29:23 -0400

    Adding unread comment count (only per page load, server based to follow)

--- files.js
+++ files.js
@@ -14,13 +14,21 @@
 // ==/UserScript==
 
 (function($) {
-    var filelist = new Array();
-    var current_index = 0;
-    var files_url_regex = /(files|commit[^s])/;
+    var filelist = new Array(),
+        viewed_comments = {
+            comments: new Array(),
+            counts: new Array()
+        },
+        current_index = 0;
+        files_url_regex = /(files|commit[^s])/;
 
     function parsePaths() {
         $('td.path a').each(function (index) {
-            filelist[index] = $(this).attr('href');
+            filelist[index] = {
+                href: $(this).attr('href'),
+                text: $(this).html(),
+                elem: $(this)
+            };
         });
     }
 
@@ -37,10 +45,10 @@
     }
 
     function showPath(index) {
-        var old_file_link = $("a[href=" + filelist[current_index] + "]"),
-            new_file_link = $("a[href=" + filelist[index] + "]"),
-            old_file = $(filelist[current_index]),
-            new_file = $(filelist[index]);
+        var old_file_link = $("a[href=" + filelist[current_index].href + "]"),
+            new_file_link = $("a[href=" + filelist[index].href + "]"),
+            old_file = $(filelist[current_index].href),
+            new_file = $(filelist[index].href);
         old_file.hide();
         old_file_link.css('font-weight', 'normal');
         new_file.show();
@@ -57,42 +65,83 @@
 
     function hideAllPaths() {
         iteratePaths(function (index, value) {
-            $(value).hide();
+            $(value.href).hide();
         });
     }
 
-    function addCommentCount() {
+    function updateCommentCount() {
         iteratePaths(function (index, value) {
-            var this_text = $("a[href=" + value + "]").first().html();
-            var comment_count = $(value + " .comment.commit-comment").length;
+            var comment_count = $(value.href + " .comment.commit-comment").length;
             if (comment_count > 0) {
-                $("a[href=" + value + "]").first().html(this_text + " (" + comment_count +  ")");
+                value.elem.html(
+                    value.text + " (" + getReadCommentCount(index) + '/' + comment_count +  ")"
+                );
             }
         });
     }
 
-    function addCommentScrollingEvent() {
+    function getReadCommentCount(index) {
+        initReadComment(index);
+        return viewed_comments.counts[index];
+    }
+
+    function initReadComment(index) {
+        if (! viewed_comments.counts[index]) {
+            viewed_comments.counts[index] = 0;
+        }
+
+        if (! viewed_comments.comments[index]) {
+            viewed_comments.comments[index] = new Array();
+        }
+    }
+
+    function setReadComment(index, comment_id) {
+        initReadComment(index);
+        if (! viewed_comments.comments[index][comment_id]) {
+            viewed_comments.comments[index][comment_id] = true;
+            viewed_comments.counts[index]++;
+        }
+    }
+
+    function updateReadCommentCounts() {
+        var docViewTop = $(window).scrollTop(),
+            docViewBottom = docViewTop + $(window).height();
+            
+        iteratePaths(function (index, value) {
+            $(value.href + ":visible .comment.commit-comment").filter(function () {
+                var thisTop = $(this).offset().top,
+                    thisBottom = thisTop + $(this).height();
+                
+                return (
+                    thisTop >= docViewTop && 
+                    thisBottom <= docViewBottom
+                );
+            }).each(function () {
+                var comment_id = parseInt($(this).attr('id').substr(1));
+                setReadComment(index, comment_id);
+            });
+        });
+        updateCommentCount();
     }
 
     function renderPaths() {
         adjustPageMargins();
         movePaths();
-        addCommentCount();
         hideAllPaths();
         showPath(current_index);
-        addCommentScrollingEvent();
+        updateReadCommentCounts();
     }
 
     function attachEvents() {
-        $('td.path a').each(function (index) {
-            filelist[index] = $(this).attr('href');
-            $(this).click(function () { 
+        iteratePaths(function (index, value) {
+            value.elem.click(function () { 
                 showPath(index);
                 return false;
             });
         });
 
         $('ul.js-hard-tabs li a').click(adjustPageMargins);
+        $(window).scroll(updateReadCommentCounts);
     }
 
     parsePaths();
